83b8d8a08da0247574f73bfccad349ea57cfbbe3
Kayla Cross <kac288@georgetown.edu>
2018-05-28 18:18:58 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,12 +1,17 @@
 CREATE TABLE project (
-  
+  id PRIMARY KEY INTEGER,
+  title TEXT,
+  cateogry
+  funding goal
+  starting date
+  ending date
 );
 
 CREATE TABLE users (
-  
+
 );
 
 CREATE TABLE pledges (
 
-  
+
 );
